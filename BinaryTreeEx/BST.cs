07f7d9a201eae2d52b4d16078c334f785edb1b5d
Fowley <alexanderfowle@gmail.com>
2020-05-01 13:32:55 -0500

    Add project files.

---
+++ BinaryTreeEx/BST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeEx
+{
+    class BST
+    {
+        // Member vars
+        Node node = new Node();
+        Node leftChild;
+        Node rightChild;
+
+
+        // ctor
+        public BST()
+        {
+
+        }
+    }
+}
---
+++ BinaryTreeEx/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeEx
+{
+    class Node
+    {
+        // Member vars
+        int value;
+    }
+}
